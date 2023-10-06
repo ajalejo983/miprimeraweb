ca7246e0e67c436bc8aea77da6d4e60c694f04fa
ajalejo983 <147113850+ajalejo983@users.noreply.github.com>
2023-10-05 20:13:31 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,25 @@
+//alert("Hola este es mi Javascript");
+
+//let nombre = "Alejandro"
+
+//var nombre2 = "Johana"
+
+//const nombre3 = "Sebastian"
+
+//console.log(nombre)
+//console.log(nombre2)
+//console.log(nombre3)
+
+let contenidoTitulo = "logo ajalejo diseño";
+
+let titulo = document.querySelector(".logo .fuente");
+titulo.innerHTML = contenidoTitulo;
+
+//CONDICIONALES
+let textoTitulo = titulo.innerText;
+console.log(textoTitulo);
+
+if(textoTitulo == "Nombre") {
+
+}
+
